a08596a1874af0fc81f69136560131a7fab199be
Jacqui Nelson <jnelson@cotingasoft.com>
2018-05-06 17:44:22 -0600

    Toggle star when clicked and refactor names of state in messages

--- src/component/Message.js
+++ src/component/Message.js
@@ -3,36 +3,46 @@ import React, { Component } from "react";
 class Message extends Component {
   constructor(props) {
     super(props);
-    this.state = { message: props.message };
+    this.state = {
+      starred: props.message.starred,
+      read: props.message.read,
+      labels: props.message.labels,
+      selected: props.message.selected,
+      subject: props.message.subject
+    };
   }
+
+  handleStarClick = () => {
+    this.setState(prevState => ({
+      starred: !prevState.starred
+    }));
+  };
+
   render() {
     return (
       <div
-        className={
-          this.state.message.read ? "row message read" : "row message unread"
-        }
+        className={this.state.read ? "row message read" : "row message unread"}
       >
         <div className="col-xs-1">
           <div className="row">
             <div className="col-xs-2">
-              <input type="checkbox" checked={this.state.message.selected} />
+              <input type="checkbox" checked={this.state.selected} />
             </div>
             <div className="col-xs-2">
               <i
                 className={
-                  this.state.message.starred
-                    ? "star fa fa-star"
-                    : "star fa fa-star-o"
+                  this.state.starred ? "star fa fa-star" : "star fa fa-star-o"
                 }
+                onClick={this.handleStarClick}
               />
             </div>
           </div>
         </div>
         <div className="col-xs-11">
-          {this.state.message.labels.map(label => (
+          {this.state.labels.map(label => (
             <span class="label label-warning">{label}</span>
           ))}
-          <a href="#">{this.state.message.subject}</a>
+          <a href="#">{this.state.subject}</a>
         </div>
       </div>
     );
